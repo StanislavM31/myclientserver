afb4c08eef8a2e8ce56b453d538ca0c63afdf51c
Stanislav Minko <minkostas@gmail.com>
2024-04-07 02:10:33 +0300

    server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const user = require("./controller/user.controller")
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(`/user`, user);
+
+app.use((err, req, res, next) => {
+    res.send(err.message);
+})
+
+module.exports = {app};
+
+
