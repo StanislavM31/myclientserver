836838a84ce7e81c27d5be5ebe5b20e7f2637e3a
Stanislav Minko <minkostas@gmail.com>
2024-04-27 22:32:09 +0300

    :service_init

---
+++ server/service/service.js
@@ -0,0 +1,22 @@
+
+async function createUser(){
+
+}
+async function getAllUsers(){
+
+}
+async function getUserById(){
+
+}
+async function updateUserById(){
+
+}
+async function deleteUserById(){
+
+}
+
+async function authorization(){
+
+}
+
+module.exports = {}
