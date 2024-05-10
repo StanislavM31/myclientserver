2bf31d1bbfbaf894e94d03e9e1074d5842642b50
Stanislav M <minkostas@gmail.com>
2024-05-10 07:15:19 +0300

    :config_ssh

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ function App() {
     console.log(value);
     //https://habr.com/ru/companies/ruvds/articles/445276/
   } */
-
+//https://git-scm.com/book/ru/v2/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%9F%D0%B5%D1%80%D0%B2%D0%BE%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F-%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0-Git
   return (
     <div className="App">
       <header className="App-header">
