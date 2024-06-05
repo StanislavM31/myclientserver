d2f9484329f7f905ddab516239231d2a543661c9
Stanislav Minko <minkostas@gmail.com>
2024-06-05 23:14:39 +0300

    :components

--- client/src/Pages/Registration/Registration.jsx
+++ client/src/Pages/Registration/Registration.jsx
@@ -39,9 +39,8 @@ fetch('http://localhost:3001/user', {
   } */
 //https://git-scm.com/book/ru/v2/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%9F%D0%B5%D1%80%D0%B2%D0%BE%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F-%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0-Git
   return (
-    <div className="App">
-      <header className="App-header">
-       {/*  <img src={logo} className="App-logo" alt="logo" /> */}
+    <div className="container">
+
         <div className="form">
           <input
             name="name"
@@ -62,13 +61,13 @@ fetch('http://localhost:3001/user', {
             placeholder="password"
           ></input>
         </div>
-        <div className="btn" onClick={sendData}>
+        <div className="btn" onClick={()=> sendData()}>
           send
         </div>
 {/*         <div className="btn" onClick={showme}>
           show
         </div> */}
-      </header>
+
     </div>
   );
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import logo from "../../logo.svg";
+import "../../App.css";
 
 
-export function Header(){
-    
-    const [value, setValue] = useState();
-
-    return 1
+export function Header() {
+  return (
+    <>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+    </>
+  );
 }
