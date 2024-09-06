067beac093485113aa0774ab4dac96da5b943650
Stanislav Minko <minkostas@gmail.com>
2024-09-06 23:58:36 +0300

    :reducer

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,12 @@ import "./App.css";
 import { useState } from "react";
 import { Header } from "./components/Header/Header.jsx";
 import { Registration } from "./Pages/Registration/Registration.jsx";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const filled = useSelector(state=> state.filled);
+  console.log(filled);
   return (
     <>
       <div className="App">
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore, Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const defaultState = {
+  filled: "all",
+};
+
+action = { type: "", payload: { name, surname, password } };
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "filled":
+      return { ...state, filled: state.filled + action.payload };
+      break;
+    case "filled":
+      return { ...state, filled: state.filled + action.payload };
+      break;
+
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
